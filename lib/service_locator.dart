03ce12f1ca810e4ca2049f80f944c4a8033672e8
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-07-13 14:35:04 +0300

    a

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:spotify_clone/data/repository/auth/auth_repository_impl.dart';
 import 'package:spotify_clone/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify_clone/domain/repository/auth/auth.dart';
+import 'package:spotify_clone/domain/usecases/auth/signup.dart';
 
 final sl = GetIt.instance;
 
@@ -9,4 +10,6 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
 
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+
+  sl.registerSingleton<SignupUseCase>(SignupUseCase());
 }
