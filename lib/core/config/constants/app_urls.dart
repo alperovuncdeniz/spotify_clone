dc7cc449ca314020b60e3e07f295c275843d168d
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-07-14 14:22:47 +0300

    a

---
+++ lib/core/config/constants/app_urls.dart
@@ -0,0 +1,5 @@
+class AppUrls {
+  static const firestorage =
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-29001.appspot.com/o/covers%2F";
+  static const mediaAlt = "alt=media";
+}
--- lib/domain/usecases/song/get_news_songs.dart
+++ lib/domain/usecases/song/get_news_songs.dart
@@ -1,11 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:spotify_clone/core/usecase/usecase.dart';
-import 'package:spotify_clone/data/repository/song/song_repository_impl.dart';
+import 'package:spotify_clone/domain/repository/song/song.dart';
 import 'package:spotify_clone/service_locator.dart';
 
 class GetNewsSongsUseCase extends UseCase<Either, dynamic> {
   @override
   Future<Either> call({params}) async {
-    return await sl<SongRepositoryImpl>().getNewsSongs();
+    return await sl<SongsRepository>().getNewsSongs();
   }
 }
